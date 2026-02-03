1e1074dc3fe061cfefe8c5e981d2fda46fffb3e7
CaffeinewithBrawl <719048@student.dusd.net>
2026-02-03 11:42:40 -0800

    Started the flashlight
    started the flashlight. Need to complete it.

---
+++ VR Room/Assets/_Course Library/Scripts/ToggleFlashlight.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleFlashlight : MonoBehaviour
+{
+    public GameObject LightSource;
+
+    private bool isOn = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
